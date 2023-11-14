4c75a877602edec4cf34bfeaf648b7e614f6d5c7
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2023-11-14 23:31:58 +0530

    docs: Changes in the documentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
         screens:{
             'widecreen' : {'raw' : '(min -aspect - ratio : 3/2)'},
             'tallscreen' : {'raw' : '(min -aspect - ratio : 13/20 )'},
+        },
+        keyframes: {
+            'open-menu':{
+                '0%' : {transform: 'scaleY(0)'},
+                '80%' : {transform: 'scaleY(0)'},
+                '100%' : {transform: 'scaleY(0)'},
+            },
+        },
+        animation: {
+            'open-menu': 'open-menu 0.5s ease-in-out forwards',
         }
     },
   },
